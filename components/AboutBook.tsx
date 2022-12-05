9c733c87cc269132753776e04f9d243ff64f4f7e
Enzo Sylvestrin <enzospavani@gmail.com>
2022-12-05 14:47:17 -0300

    Correção

--- components/AboutBook.tsx
+++ components/AboutBook.tsx
@@ -1,28 +1,30 @@
-import { Container, ContainerImg, ContainerRe, Description, WrapperRe } from '../styles/HomeStyled';
+import { Container, ContainerImg, Description } from '../styles/HomeStyled';
+
+import ReiArthur from '../public/assets/ReiArthurImg.jpg';
 
 type IAbout = {
-    titulo: string;
-    subtitulo?: string;
-    sinopse: string;
-    Autor: string;
-    cor: string;
-    imagem: string;
+  titulo: string;
+  subtitulo?: string;
+  sinopse: string;
+  Autor: string;
+  cor: string;
+  imagem: string;
 }
 
 const AboutBook = (props : IAbout) => {
     <Container>
-        <Description>
-          <h1>{props.titulo}</h1>
-          <p>{props.subtitulo}</p>
-          <span>{props.sinopse}</span>
+      <Description>
+        <h1>{props.titulo}</h1>
+        {props.subtitulo ? <p>{props.subtitulo}</p> : <></>}
+        <span>{props.sinopse}</span>
 
-          <h3 style={{color: props.cor}}>Autor: {props.Autor}</h3>
+        <h3 style={{color: props.cor}}>Autor: {props.Autor}</h3>
 
-        </Description>
-        <ContainerImg>
-          <img src={props.imagem} alt="Capa do livro" />
-        </ContainerImg>
-      </Container>
+      </Description>
+      <ContainerImg>
+        <img src={ReiArthur.src} alt="Capa do livro" />
+      </ContainerImg>
+    </Container>
 }
 
 export default AboutBook;
